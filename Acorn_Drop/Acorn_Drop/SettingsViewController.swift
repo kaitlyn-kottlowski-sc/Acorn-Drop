c9e61bbafbb98a367772032a23226af740c4821e
j-luang <jesselua5@gmail.com>
2019-11-06 11:09:45 -0600

    Linked other views

---
+++ Acorn_Drop/Acorn_Drop/SettingsViewController.swift
@@ -0,0 +1,49 @@
+//
+//  SettingsViewController.swift
+//  Acorn_Drop
+//
+//  Created by Jesse Luangaphayvong on 11/6/19.
+//  Copyright © 2019 Jesse Luangaphayvong. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController
+{
+    public var someAttribute = 0
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        print("Some attribute =  \(someAttribute)")
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func goBackToTitle(_ sender: Any)
+    {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue,
+                          sender: Any?)
+    {
+        if let destinationViewController =
+            segue.destination as? ViewController
+        {
+            destinationViewController.attribute = 2
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Acorn_Drop/Acorn_Drop/ViewController.swift
+++ Acorn_Drop/Acorn_Drop/ViewController.swift
@@ -8,13 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
+    public var attribute = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    override func prepare(for segue: UIStoryboardSegue,                                 sender: Any?)
+    {
+        if let destinationViewController =                                 segue.destination as? SettingsViewController
+        {
+            destinationViewController.someAttribute = 5
+        }
+    }
+    
+    @IBAction func unwindFromSettings(sender:
+        UIStoryboardSegue)
+    {
+        print("Attribute = \(attribute)")
+        // This is a public attribute in viewcontroller A.
+        // Its value will be set in viewcontroller B.
+    }
 
 }
 
