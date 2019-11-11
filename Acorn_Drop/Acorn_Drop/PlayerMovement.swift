fd1ce4e5fa194369f4c6b20c8b81cf14163dfc4f
Jesse Luangaphayvong <jesselua5@gmail.com>
2019-11-11 09:34:10 -0600

    scoring, error fixes

--- Acorn_Drop/Acorn_Drop/PlayerMovement.swift
+++ Acorn_Drop/Acorn_Drop/PlayerMovement.swift
@@ -6,80 +6,62 @@
 //  Copyright © 2019 Jesse Luangaphayvong. All rights reserved.
 //
 
-import Foundation
 import SpriteKit
 import GameplayKit
 
-class PlayerMovement: SKScene
+class PlayerMovement: SKScene, SKPhysicsContactDelegate
 {
-    //emitternode load background
-    var backgroundPicture: SKEmitterNode!
-    var player = SKSpriteNode?()
-    var playerColor = UIColor.orangeColor()
-    var backGroundColor = UIColor(red: 0.2, green: 0.2, blue: 0.2, alpha: 1.0)
+    var backgroundPicture: SKEmitterNode
+    var player : SKSpriteNode
+    var playerColor = UIColor.orange
+    //var backgroundColor = UIColor.blue
     var playerSize = CGSize(width: 50, height: 50)
-    var touchLocation = CGPoint?()
-
-    override func didMoveToView(view: SKView)
-    {
+    var scoreLabel: SKLabelNode
+       var score: Int = 0
+       {
+           didSet
+           {
+                   scoreLabel.text = "Score: \(score)"
+           }
+       }
+    
+    override func didMove(to view: SKView) {
         // set background
         // background = SKEmitterNode(fileNamed:"")
-        self.backGroundColor = backGroundColor
+        //self.backGroundColor = backgroundColorHolder
         spawnPlayer()
+        
+        self.physicsWorld.gravity = CGVector(dx: 0, dy: 0)
+        self.physicsWorld.contactDelegate = self as? SKPhysicsContactDelegate
+        
+        scoreLabel = SKLabelNode(text: "Score: 0")
+        scoreLabel.position = CGPoint(x: 100, y: self.frame.size.height - 60)
+        scoreLabel.fontName = "AmericanTypewriter-Bold"
+        
+        scoreLabel.fontSize = 36
+        scoreLabel.fontColor = UIColor.white
+        score = 0
+        self.addChild(scoreLabel)
     }
-
+    
     func spawnPlayer()
     {
+        //first line is place holder, replace color with imageNamed: "FileString"
         player = SKSpriteNode(color: playerColor, size: playerSize)
-        player?.position = CGPoint(x: CGRectGetMidX(self.frame), y: CGRectGetMidY(self.frame) - 200)
-        self.addChild(player!)
+        player.position = CGPoint(x: self.frame.midX, y: self.frame.midY - 200)
+        self.addChild(player)
     }
     
-    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?)
-    {
+    func touchMoved(touches: Set<UITouch>, withEvent event: UIEvent) {
         for touch in touches
         {
-            let touchLocation = touch.locationInNode(self)
-            movePlayer()
-        }
-    }
-
-    func movePlayer()
-    {
-        player?.position.x = touchLocation?.x
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
+            let touchLocation = touch.location(in: self)
+            player.position.x = touchLocation.x
         }
     }
     
-        /*func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-        func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-        func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-        func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }*/
-    
-    
-        func update(_ currentTime: TimeInterval) {
+    override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
     }
-}
+
 }
