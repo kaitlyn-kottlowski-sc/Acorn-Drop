46a8e2c5f1eadfd28829c9aaa6ca4c94f9834f32
jesse.luangaphayvong <jesse.luangaphayvong@my.simpson.edu>
2019-11-11 08:40:23 -0600

    player movement/background

--- Acorn_Drop/Acorn_Drop/PlayerMovement.swift
+++ Acorn_Drop/Acorn_Drop/PlayerMovement.swift
@@ -12,45 +12,41 @@ import GameplayKit
 
 class PlayerMovement: SKScene
 {
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    //Initalize and create whatever
-    override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+    //emitternode load background
+    var backgroundPicture: SKEmitterNode!
+    var player = SKSpriteNode?()
+    var playerColor = UIColor.orangeColor()
+    var backGroundColor = UIColor(red: 0.2, green: 0.2, blue: 0.2, alpha: 1.0)
+    var playerSize = CGSize(width: 50, height: 50)
+    var touchLocation = CGPoint?()
+
+    override func didMoveToView(view: SKView)
+    {
+        // set background
+        // background = SKEmitterNode(fileNamed:"")
+        self.backGroundColor = backGroundColor
+        spawnPlayer()
+    }
+
+    func spawnPlayer()
+    {
+        player = SKSpriteNode(color: playerColor, size: playerSize)
+        player?.position = CGPoint(x: CGRectGetMidX(self.frame), y: CGRectGetMidY(self.frame) - 200)
+        self.addChild(player!)
     }
     
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?)
+    {
+        for touch in touches
+        {
+            let touchLocation = touch.locationInNode(self)
+            movePlayer()
         }
+    }
+
+    func movePlayer()
+    {
+        player?.position.x = touchLocation?.x
     }
     
     func touchUp(atPoint pos : CGPoint) {
