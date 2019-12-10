9f84b118b95c34c60fd22b1712013bf027174e41
treychumbley <trey.chumbley@my.simpson.edu>
2019-12-10 10:02:49 -0600

    moved game over button

--- Acorn_Drop/Acorn_Drop/GameOverViewController.swift
+++ Acorn_Drop/Acorn_Drop/GameOverViewController.swift
@@ -16,15 +16,7 @@ class GameOverViewController: UIViewController {
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        if let view = self.view as! SKView? {
-                    // Load the SKScene from 'GameScene.sks'
-                    if let scene = SKScene(fileNamed: "GameOverScene") {
-                        // Set the scale mode to scale to fit the window
-                        scene.scaleMode = .aspectFill
-                         //Present the scene
-                        view.presentScene(scene)
-                    }
-        }
+
     }
     
 
